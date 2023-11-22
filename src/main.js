c053283780a7612e103818531e06d7c8ef362d44
Acauã Montiel <contato@acauamontiel.com.br>
2023-11-22 11:45:29 -0300

    refactor: rename and remove files #86a1c2c4z

--- src/main.js
+++ src/main.js
@@ -6,12 +6,12 @@ import PrimeVue from 'primevue/config';
 
 import App from './App.vue';
 import router from './router';
-import i18n from './locale';
+import i18n from './i18n';
 
 const app = createApp(App);
 
+app.use(PrimeVue, {ripple: true});
 app.use(router);
 app.use(i18n);
-app.use(PrimeVue, {ripple: true});
 
 app.mount('#app');
