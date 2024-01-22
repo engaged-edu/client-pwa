4b2871e4f51f10dec06c57089ebf1b1bc9c983ee
Acauã Montiel <contato@acauamontiel.com.br>
2024-01-22 16:56:27 -0300

    feat: add bank slip tab #86a1tg5n7

--- src/views/MethodBankSlip.vue
+++ src/views/MethodBankSlip.vue
@@ -1,3 +1,16 @@
 <template lang="pug">
-h1 bank slip
+.flex.flex-column.align-items-center
+	.flex.justify-content-center.align-items-center.gap-4.my-5
+		.flex.align-items-center.gap-2
+			IconBankSlip
+			span {{ $t('payment.methods.bankSlip') }}
+		.text-sm {{ $t('payment.amount') }}:&nbsp;
+			span.font-bold {{ $t('cur', [invoice.amount, invoice.currency]) }}
+
+	.text-sm.text-color-secondary.max-w-30rem.text-center.line-height-3 {{ $t('payment.bankSlipInstructions') }}
 </template>
+
+<script setup>
+const invoice = inject('invoice');
+
+</script>
