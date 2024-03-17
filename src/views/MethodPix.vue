734395296fcfd6463e3ea210e7e073ea3ce89404
Acauã Montiel <contato@acauamontiel.com.br>
2024-03-17 00:59:54 -0300

    fix: pix instructions width #86a2p1vqb

--- src/views/MethodPix.vue
+++ src/views/MethodPix.vue
@@ -8,18 +8,22 @@
 			span.font-bold {{ $t('cur', [invoice.amount, invoice.currency]) }}
 
 	.text-sm.text-color-secondary.flex.flex-column.max-w-30rem.gap-4
-		.flex.align-items-center
-			i-mdi-numeric-1-circle.w-1.h-2rem.p-1
-			.w-9(v-html="$t('payment.pixInstructions[0]')")
-		.flex.align-items-center
-			i-mdi-numeric-2-circle.w-1.h-2rem.p-1
-			.w-9 {{ $t('payment.pixInstructions[1]') }}
-		.flex.align-items-center
-			i-mdi-numeric-3-circle.w-1.h-2rem.p-1
-			.w-9 {{ $t('payment.pixInstructions[2]') }}
-		.flex.align-items-center
-			i-mdi-numeric-4-circle.w-1.h-2rem.p-1
-			.w-9 {{ $t('payment.pixInstructions[3]') }}
+		.flex.align-items-center.gap-2
+			div
+				i-mdi-numeric-1-circle.w-2rem.h-2rem.p-1
+			div(v-html="$t('payment.pixInstructions[0]')")
+		.flex.align-items-center.gap-2
+			div
+				i-mdi-numeric-2-circle.w-2rem.h-2rem.p-1
+			div {{ $t('payment.pixInstructions[1]') }}
+		.flex.align-items-center.gap-2
+			div
+				i-mdi-numeric-3-circle.w-2rem.h-2rem.p-1
+			div {{ $t('payment.pixInstructions[2]') }}
+		.flex.align-items-center.gap-2
+			div
+				i-mdi-numeric-4-circle.w-2rem.h-2rem.p-1
+			div {{ $t('payment.pixInstructions[3]') }}
 </template>
 
 <script setup>
