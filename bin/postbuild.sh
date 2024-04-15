6558a66a960c21a913fdc5bf37df7b35799e4ded
Acauã Montiel <contato@acauamontiel.com.br>
2024-04-15 16:06:00 -0300

    chore: test puppeteer #86a30c6m1

--- bin/postbuild.sh
+++ bin/postbuild.sh
@@ -5,6 +5,7 @@ rm -rf ./.amplify-hosting
 mkdir -p ./.amplify-hosting/compute
 
 cp -r ./dist ./.amplify-hosting/compute/default
+cp ./src/server.mjs ./.amplify-hosting/compute/default
 cp -r ./node_modules ./.amplify-hosting/compute/default/node_modules
 
 cp -r public ./.amplify-hosting/static
