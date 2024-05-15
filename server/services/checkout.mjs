3ed83d83a23b9e9fec0548efa77257701447e515
Acauã Montiel <contato@acauamontiel.com.br>
2024-05-15 18:20:40 -0300

    fix: description field #86a30cvuh

--- server/services/checkout.mjs
+++ server/services/checkout.mjs
@@ -1,18 +1,23 @@
 import { fetchGQL } from '#server/services/index.mjs';
 import { publicFetchCheckout } from '#server/graphql/checkout.mjs';
 
+export function setContent(organization, product) {
+	const { appearance } = organization;
+
+	return {
+		title: `${organization.name} - ${product?.product?.name || product.name}`,
+		description: product?.product?.description,
+		favicon: appearance?.squareLogo32?.url,
+		image: appearance?.logo?.url || appearance?.squareLogo256?.url
+	};
+}
+
 export default {
 	async getCheckout(req) {
 		const response = await fetchGQL(publicFetchCheckout, { checkoutSharedId: req.params.id });
 		const data = response.data.publicFetchCheckout;
 		const [ product ] = data.invoiceItems;
-		const { appearance } = data.organization;
 
-		return {
-			title: `${data.organization.name} - ${product?.product?.name || product.name}`,
-			description: product?.description,
-			favicon: appearance?.squareLogo32?.url,
-			image: appearance?.logo?.url || appearance?.squareLogo256?.url
-		};
+		return setContent(data.organization, product);
 	}
 };
--- server/services/invoice.mjs
+++ server/services/invoice.mjs
@@ -1,4 +1,5 @@
 import { fetchGQL } from '#server/services/index.mjs';
+import { setContent } from '#server/services/checkout.mjs';
 import { publicFetchInvoicePaymentLink } from '#server/graphql/invoice.mjs';
 
 export default {
@@ -6,13 +7,7 @@ export default {
 		const response = await fetchGQL(publicFetchInvoicePaymentLink, { accessToken: req.query.accessToken });
 		const data = response.data.publicFetchInvoicePaymentLink;
 		const [ product ] = data.invoice.items;
-		const { appearance } = data.organization;
 
-		return {
-			title: `${data.organization.name} - ${product?.product?.name || product.name}`,
-			description: product?.description,
-			favicon: appearance?.squareLogo32?.url,
-			image: appearance?.logo?.url || appearance?.squareLogo256?.url
-		};
+		return setContent(data.organization, product);
 	}
 };
